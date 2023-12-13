3390dea72bc78f5a26cd40a3654236d35d7d76cf
fwwz-id <fwwz.id@proton.me>
2023-12-13 23:38:23 +0700

    chore: fix typo on some file

--- src/routes/api/v1/Content.router.js
+++ src/routes/api/v1/Content.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import expressAsyncHandler from "express-async-handler";
 
 import ContentController from "../../../controller/v1/Content.controller";
 
 const router = Router();
 
-router.get("/contents", ContentController.index);
+router.get("/contents", expressAsyncHandler(ContentController.index));
 
 export default router;
